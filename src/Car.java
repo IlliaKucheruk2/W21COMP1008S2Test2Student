177bb2ef7c594a8e1a48acef82226a9aac13f295
IlliaKucheruk200453638 <77804360+IlliaKucheruk200453638@users.noreply.github.com>
2021-03-04 12:26:18 -0500

    Initial commit

---
+++ src/Car.java
@@ -0,0 +1,48 @@
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Name:Illia Kucheruk
+ * Student number: 200453638
+ */
+public class Car {
+    private String make, model;
+    private int mileage;
+    private double price;
+
+    public Car(String make, String model, int mileage, double price) {
+    }
+
+    public String getMake() {
+        return make;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public int getMileage() {
+        return mileage;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setMake(String make) {
+    }
+
+    public void setModel(String model) {
+    }
+
+    public void setMileage(int mileage) {
+    }
+
+    public void setPrice(double price) {
+    }
+
+    public String toString()
+    {
+        return "";
+    }
+}
