dfba3162198a20c309714d283150f4cf88044841
IlliaKucheruk200453638 <77804360+IlliaKucheruk200453638@users.noreply.github.com>
2021-03-04 14:50:15 -0500

    question 14,15,16 complete!

--- src/CarLot.java
+++ src/CarLot.java
@@ -1,7 +1,10 @@
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class CarLot {
     private ArrayList<Car> cars;
+    int number_of_cars = 0;
 
     public CarLot(){
         cars = new ArrayList<>();
@@ -25,7 +28,10 @@ public class CarLot {
 
     public ArrayList<Car> getCarsByBrand(String manufacturer)
     {
-        return null;
+        String[] car= new String[] {"Ford", "Honda","Porsche"};
+        List<String> carsList = Arrays.asList(car);
+        String info="";
+        return ("");
     }
 
     public ArrayList<Car> getCarsByModel(String model)
@@ -50,16 +56,27 @@ public class CarLot {
 
     public double getAvgPrice()
     {
-        return -1;
+        double priceAvarage = 0;
+        double avarage= 0;
+
+        for (int i = 0; i < cars.size(); i++) {
+            priceAvarage += cars.get(i).getPrice();
+        }
+        avarage= priceAvarage/cars.size();
+        return avarage;
     }
 
     public int getNumOfCarsInInventory()
     {
-        return -1;
+
+        for (int i = 0; i < cars.size(); i++) {
+            number_of_cars++;
+        }
+        return number_of_cars;
     }
 
     public String toString()
     {
-        return "";
+        return String.format("We have %1s cars worth $%2s", number_of_cars, inventory_value);
     }
 }
